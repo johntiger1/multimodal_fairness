d490d77d562edc59336197c256d149d4f76f47d2
John Chen <john.person1@gmail.com>
2020-05-04 21:41:21 -0400

    add remaining scripts for data generation to master script

---
+++ script.sh
@@ -0,0 +1,4 @@
+python -m mimic3benchmark.scripts.create_decompensation data/root/ data/decompensation/
+python -m mimic3benchmark.scripts.create_length_of_stay data/root/ data/length-of-stay/
+python -m mimic3benchmark.scripts.create_phenotyping data/root/ data/phenotyping/
+python -m mimic3benchmark.scripts.create_multitask data/root/ data/multitask/
