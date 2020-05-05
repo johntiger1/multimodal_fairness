77530985d5c6651085098ee1ca8e3177be883523
John Chen <john.person1@gmail.com>
2020-05-05 18:13:43 -0400

    extracting the key information

--- src/playing_with_readers.py
+++ src/playing_with_readers.py
@@ -4,4 +4,9 @@ reader = DecompensationReader(dataset_dir='data/decompensation/train',
                               listfile='data/decompensation/train/listfile.csv')
 
 print(reader.read_example(10))
-print(reader.read_example(10)["name"]) # the name has what we want. now we can go and join stuff with text
+print("keys??")
+print("{} KEY is {}".format(
+
+    "ICUSTAY_ID", reader.read_example(10)["name"].split("_")[0]
+
+)) # the name has what we want. now we can go and join stuff with text
