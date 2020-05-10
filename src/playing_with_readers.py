d9a239fed2c614a348666b2b150d1f72f529003b
John Chen <john.person1@gmail.com>
2020-05-09 23:22:01 -0400

    reach end of file, moving to jupyter notebook

--- src/playing_with_readers.py
+++ src/playing_with_readers.py
@@ -19,19 +19,43 @@ print("so we have this 10th example. Now, what do we do to it?")
 print(reader.read_example(10)["name"])
 patient_id = reader.read_example(10)["name"].split("_")[0]
 MIMIC_ROOT = "data/root/train/"
+MIMIC_og_data_ROOT = "data/physionet.org/files/mimiciii/1.4/"
+notes_table = "NOTEEVENTS.csv"
 import os
 
 with open(os.path.join(MIMIC_ROOT, patient_id, "stays.csv"), "r") as file:
     print("finding relevant info for {}".format(patient_id))
+    entries = []
     for line in file:
-        print(line)
+        stuff = line.split(",")
+        print(stuff)
+        entries.append(stuff[0:3])
+    entries = entries[1:]
     print("why are there two things in here? Because there are two episodes")
     print("the reason is that. the mortality prediction only uses 1 episode. The reason is that they may remove or invalidate parts of it for the mortality prediction"
           "For instance! the stay may have been < 48 hours"
           "but the decompensation prediction episode for examples uses 2 episodes")
-
+    # subj_id =
+    # hadm_id = bb
+    # icustay_id = cc
     print("now, we will do some interesting joining!")
+    df = pd.read_csv(os.path.join(MIMIC_og_data_ROOT, notes_table))
+    df.CHARTDATE = pd.to_datetime(df.CHARTDATE)
+    df.CHARTTIME = pd.to_datetime(df.CHARTTIME)
+    df.STORETIME = pd.to_datetime(df.STORETIME)
 
+    df2 = df[df.SUBJECT_ID.notnull()]
+    df2 = df2[df2.HADM_ID.notnull()]
+    df2 = df2[df2.CHARTTIME.notnull()]
+    df2 = df2[df2.TEXT.notnull()]
+
+    df2 = df2[['SUBJECT_ID', 'HADM_ID', 'CHARTTIME', 'TEXT']]
+
+    del df
+    notes_df = df2
+    specific_visit = list(map(lambda x: int (x), entries[0]))
+    print(notes_df[(notes_df['SUBJECT_ID'] == specific_visit[0]) & (notes_df['HADM_ID'] == specific_visit[1])])
+    logging.debug("Moving to jupyter notebook for max understanding")
 # with open(os.path.join(MIMIC_ROOT, str(99999), "stays.csv"), "r") as file:
 #     print("finding relevant info for {}".format(99999))
 #     for line in file:
