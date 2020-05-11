20b197e5dc80c32a8521bdfde18f9455283777d4
John Chen <john.person1@gmail.com>
2020-05-11 15:30:52 -0400

    Successfully generate the notes now, per patient

--- src/preprocessing/extract_notes.py
+++ src/preprocessing/extract_notes.py
@@ -3,7 +3,9 @@ import pandas as pd
 import logging
 import glob
 import os
+import pandas as pd
 
+from tqdm.auto import  tqdm
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 logger.debug("hello")
@@ -22,11 +24,45 @@ notes).
 '''
 Extracts notes, going over the TOTAL dataset
 '''
-def extract_notes(data_path="data/root"):
+def extract_notes(data_path="data/root", output_dir ="data/extracted_notes"):
 
+    '''
+    load a copy of the notes CSV for processing
+
+    '''
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+
+    notes_table = pd.read_csv("data/physionet.org/files/mimiciii/1.4/NOTEEVENTS.csv")
+    # logging.debug(notes_table.dtypes)
+    total = 0
     for dir, subdir, file in os.walk(data_path):
-        for split in subdir:
-            print(split)
+        total+=1
+        for split in tqdm(subdir, total=100000):
+            if split.isdigit():
+                patient_id = int(split)
+                # logging.debug("{} {} {}".format(dir,subdir, file))
+                # logging.info("{} {}".format(dir, patient_id))
+        #         now, for each specific episode, extract the hadm
+                with open(os.path.join(dir, split, "stays.csv")) as stays_file:
+                    stays_file.readline() #consume the first line
+                    # for line in stays_file:
+                    hadm_id = int(stays_file.readline().split(",")[1])
+                        # break
+                relevant_notes_idx = (notes_table["HADM_ID"] == hadm_id) &\
+                                     (notes_table["SUBJECT_ID"] == patient_id)
+
+                output_subdir = os.path.join(output_dir, str(patient_id))
+                if not os.path.exists(output_subdir):
+                    os.makedirs(output_subdir, exist_ok=True)
+
+                output_location = os.path.join(output_subdir, "notes.pkl")
+                notes_table[ relevant_notes_idx].to_pickle(output_location)
+
+
+        logging.info("{} entries processed".format(total))
+
         # patient_id = subdir.split(os.path.pathsep)[-1]
         # print(patient_id)
 
