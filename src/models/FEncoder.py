fc15df37801cdc8d40c6f66c77673d33feb23b4f
John Chen <john.person1@gmail.com>
2020-05-13 16:03:18 -0400

    demonstrate, we are working off the local repo of Fconv

---
+++ src/models/FEncoder.py
@@ -0,0 +1,18 @@
+
+
+# testing the FEncoder
+# from fairseq.fairseq.models
+
+import fairseq
+# import  fairseq.fairseq.models.
+# import  fairseq
+
+# import torch
+# print(torch.__version__)
+
+# fair
+fCE = fairseq.models.fconv.FConvEncoder(None)
+# print("ok")
+
+# print(fCE)
+
