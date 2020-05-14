22be3d7c56e33c566b71f8dce294232c76b63d8e
John Chen <john.person1@gmail.com>
2020-05-13 22:19:15 -0400

    path imports must be from the repo root?

---
+++ src/models/allen_mort_pred/text_mortality/__init__.py
@@ -0,0 +1,5 @@
+from src.models.allen_mort_pred.text_mortality.models import *
+
+# from text_mortality.dataset_readers import *
+# from my_library.predictors import *
+# src/models/allen_mort_pred/text_mortality
