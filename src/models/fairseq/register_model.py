65bf227b176aa51f5c96509e7910b00b2de49628
John Chen <john.person1@gmail.com>
2020-05-14 19:43:08 -0400

    add register model code. THIS IS AN EXAMPLE WHERE pycharm and actual imports DO NOT AGREE

---
+++ src/models/fairseq/register_model.py
@@ -0,0 +1,76 @@
+from fairseq.models import FairseqEncoderDecoderModel, register_model
+from simple_lstm import SimpleLSTMDecoder
+# Note: the register_model "decorator" should immediately precede the
+# definition of the Model class.
+
+@register_model('simple_lstm')
+class SimpleLSTMModel(FairseqEncoderDecoderModel):
+
+    @staticmethod
+    def add_args(parser):
+        # Models can override this method to add new command-line arguments.
+        # Here we'll add some new command-line arguments to configure dropout
+        # and the dimensionality of the embeddings and hidden states.
+        parser.add_argument(
+            '--encoder-embed-dim', type=int, metavar='N',
+            help='dimensionality of the encoder embeddings',
+        )
+        parser.add_argument(
+            '--encoder-hidden-dim', type=int, metavar='N',
+            help='dimensionality of the encoder hidden state',
+        )
+        parser.add_argument(
+            '--encoder-dropout', type=float, default=0.1,
+            help='encoder dropout probability',
+        )
+        parser.add_argument(
+            '--decoder-embed-dim', type=int, metavar='N',
+            help='dimensionality of the decoder embeddings',
+        )
+        parser.add_argument(
+            '--decoder-hidden-dim', type=int, metavar='N',
+            help='dimensionality of the decoder hidden state',
+        )
+        parser.add_argument(
+            '--decoder-dropout', type=float, default=0.1,
+            help='decoder dropout probability',
+        )
+
+    @classmethod
+    def build_model(cls, args, task):
+        # Fairseq initializes models by calling the ``build_model()``
+        # function. This provides more flexibility, since the returned model
+        # instance can be of a different type than the one that was called.
+        # In this case we'll just return a SimpleLSTMModel instance.
+
+        # Initialize our Encoder and Decoder.
+        encoder = SimpleLSTMEncoder(
+            args=args,
+            dictionary=task.source_dictionary,
+            embed_dim=args.encoder_embed_dim,
+            hidden_dim=args.encoder_hidden_dim,
+            dropout=args.encoder_dropout,
+        )
+        decoder = SimpleLSTMDecoder(
+            dictionary=task.target_dictionary,
+            encoder_hidden_dim=args.encoder_hidden_dim,
+            embed_dim=args.decoder_embed_dim,
+            hidden_dim=args.decoder_hidden_dim,
+            dropout=args.decoder_dropout,
+        )
+        model = SimpleLSTMModel(encoder, decoder)
+
+        # Print the model architecture.
+        print(model)
+
+        return model
+
+    # We could override the ``forward()`` if we wanted more control over how
+    # the encoder and decoder interact, but it's not necessary for this
+    # tutorial since we can inherit the default implementation provided by
+    # the FairseqEncoderDecoderModel base class, which looks like:
+    #
+    # def forward(self, src_tokens, src_lengths, prev_output_tokens):
+    #     encoder_out = self.encoder(src_tokens, src_lengths)
+    #     decoder_out = self.decoder(prev_output_tokens, encoder_out)
+    #     return decoder_out
