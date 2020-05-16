fix dim issue with softmax

src/models/new_allen_nlp/MortalityModelDemo.py
@@ -201,7 +201,7 @@ class MortalityClassifier(Model):
         # Shape: (batch_size, num_labels)
         logits = self.classifier(encoded_text)
         # Shape: (batch_size, num_labels)
-        probs = torch.nn.functional.softmax(logits)
+        probs = torch.nn.functional.softmax(logits, dim=-1)
         # Shape: (1,)
         loss = torch.nn.functional.cross_entropy(logits, label)
         self.accuracy(logits, label)