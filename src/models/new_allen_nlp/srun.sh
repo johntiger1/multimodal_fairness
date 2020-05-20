2faa60a79376a276977d4928b5de0c9692fc9b5b
John Chen <john.person1@gmail.com>
2020-05-20 16:37:58 -0400

    update srun script for mortality

--- src/models/new_allen_nlp/srun.sh
+++ src/models/new_allen_nlp/srun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source activate hurtfulwords
-srun --mem=3g -p gpu --gres=gpu:1 --unbuffered  python ClassifyModelDemo.py
+srun --mem=12g -p gpu --gres=gpu:1 --unbuffered  python MortalityModelDemo.py
