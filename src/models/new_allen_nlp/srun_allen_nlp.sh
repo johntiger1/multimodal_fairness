ef13383fb715fefc106570398610c60cfa4a3d11
John Chen <john.person1@gmail.com>
2020-05-22 12:12:39 -0400

    Fix script by properly dereferncing variable

--- src/models/new_allen_nlp/srun_allen_nlp.sh
+++ src/models/new_allen_nlp/srun_allen_nlp.sh
@@ -2,5 +2,5 @@
 source activate hurtfulwords
 SER_DIR="experiments/20-config"
 rm -rf $SER_DIR
-srun --mem=30g -c 4 -p gpu --gres=gpu:1 --unbuffered  allennlp train MortalityClassifier.jsonnet -s SER_DIR --include-package MortalityClassifier
+srun --mem=30g -c 4 -p gpu --gres=gpu:1 --unbuffered  allennlp train MortalityClassifier.jsonnet -s $SER_DIR --include-package MortalityClassifier
 
