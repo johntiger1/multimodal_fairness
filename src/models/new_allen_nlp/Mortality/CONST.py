c9f2dfc046f453c6fc31aa68a5213c8e91a0c609
John Chen <john.person1@gmail.com>
2020-05-25 11:48:05 -0400

    dynamically set attributes on args

--- src/models/new_allen_nlp/Mortality/CONST.py
+++ src/models/new_allen_nlp/Mortality/CONST.py
@@ -1,1 +1,16 @@
 LOGGER_NAME = "decompensation_logger"
+
+# now, we will set the stuff on the args appropriately (a seamless merge)
+decomp_config = {
+
+    "key1" : 23,
+    "key2": 24 #reinventing the json and config
+
+}
+args = lambda x: None
+for key,val in (decomp_config).items():
+    print(f"{key} {val}\n")
+
+    args.__setattr__(key, val)
+
+print(args)
