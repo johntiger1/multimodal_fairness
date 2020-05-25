bee97006b32ea47453d017e9ba46bac7a7ce76dd
John Chen <john.person1@gmail.com>
2020-05-25 16:51:26 -0400

    switch to decomp; pane setting works in lxterminal too

--- src/models/new_allen_nlp/Mortality/srun.sh
+++ src/models/new_allen_nlp/Mortality/srun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source activate hurtfulwords
-RUN_NAME="55-named_srun"
-printf '\033]2;%s\033\\' $RUN_NAME
-srun --mem=15g -c 8 -p gpu --gres=gpu:1 --unbuffered  python MortalityMain.py --run_name=$RUN_NAME
+RUN_NAME="60-preproc-decomp-50k"
+printf '\033]2;%s\033\\' "$RUN_NAME"
+srun --mem=60g -c 4 -p gpu --gres=gpu:1 --unbuffered  python MortalityMain.py --run_name="$RUN_NAME"
