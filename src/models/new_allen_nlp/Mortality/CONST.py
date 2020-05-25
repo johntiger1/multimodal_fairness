afdaa8743e5da5ddcd234b70e68ec1536799a7d0
John Chen <john.person1@gmail.com>
2020-05-25 12:06:14 -0400

    add decomp and mort configs

--- src/models/new_allen_nlp/Mortality/CONST.py
+++ src/models/new_allen_nlp/Mortality/CONST.py
@@ -1,16 +1,35 @@
 LOGGER_NAME = "decompensation_logger"
 
 # now, we will set the stuff on the args appropriately (a seamless merge)
-decomp_config = {
 
-    "key1" : 23,
-    "key2": 24 #reinventing the json and config
+phenotyping_config = {
+
 
 }
-args = lambda x: None
-for key,val in (decomp_config).items():
-    print(f"{key} {val}\n")
 
-    args.__setattr__(key, val)
+mortality_config = {
 
-print(args)
+"train_data": "/scratch/gobi1/johnchen/new_git_stuff/multimodal_fairness/data/in-hospital-mortality/train/listfile.csv",
+"dev_data" : "/scratch/gobi1/johnchen/new_git_stuff/multimodal_fairness/data/in-hospital-mortality/test/listfile.csv",
+"data_type": "MORTALITY"
+}
+
+decomp_config = {
+
+    "train_data": "/scratch/gobi1/johnchen/new_git_stuff/multimodal_fairness/data/decompensation/train/listfile.csv",
+    "dev_data": "/scratch/gobi1/johnchen/new_git_stuff/multimodal_fairness/data/decompensation/test/listfile.csv",
+    "data_type": "DECOMPENSATION"
+
+}
+configs = {"phenotyping_config": phenotyping_config,
+           "mortality_config": mortality_config,
+           "decomp_config": decomp_config}
+
+def set_config(name, obj_to_set):
+    for key, val in (configs[name]).items():
+        print(f"{key} {val}\n")
+
+        obj_to_set.__setattr__(key, val)
+    return obj_to_set
+
+
