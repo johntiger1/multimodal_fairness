7d10917d6a33ee553a46d1300526ca3044aa6024
John Chen <john.person1@gmail.com>
2020-05-25 14:48:42 -0400

    integrate tmux panel titling with script

--- src/models/new_allen_nlp/Mortality/srun.sh
+++ src/models/new_allen_nlp/Mortality/srun.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source activate hurtfulwords
-srun --mem=30g -c 4 -p gpu --gres=gpu:1 --unbuffered  python MortalityMain.py
+RUN_NAME="55-named_srun"
+printf '\033]2;%s\033\\' $RUN_NAME
+srun --mem=15g -c 8 -p gpu --gres=gpu:1 --unbuffered  python MortalityMain.py --run_name=$RUN_NAME
