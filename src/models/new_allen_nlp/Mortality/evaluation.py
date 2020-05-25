b5e6277f17a44ae0e061e49dcc647f493ee88287
John Chen <john.person1@gmail.com>
2020-05-25 16:32:00 -0400

    add tqdm for large predictions csv generation

--- src/models/new_allen_nlp/Mortality/evaluation.py
+++ src/models/new_allen_nlp/Mortality/evaluation.py
@@ -9,22 +9,22 @@ import logging
 from sklearn import metrics
 logger = logging.getLogger(__name__)
 
-
+from tqdm.auto import tqdm
 
 def compute_auc(predictions_path: str=None):
     # pd readcsv each of those, concat, then we pass in the data to AUCROC
     #
     all_preds_df = pd.DataFrame()
     for root, dirs, files in os.walk(predictions_path):
-        for file in files:
+        for file in tqdm(files, total=500000//256):
             if str(file).startswith("predictions_"):
-                logger.critical(f"{file}\n")
+                # logger.critical(f"{file}\n")
                 preds = pd.read_csv(os.path.join(root,file))
                 all_preds_df = pd.concat((all_preds_df, preds), axis=0)
 
     all_preds_df["predictions"] = all_preds_df.apply(lambda row:  1 if row["probs_1"] > row["probs_0" ] else 0, axis=1)
     print(metrics.roc_auc_score(all_preds_df["label"], all_preds_df["probs_1"] ))
-
+    all_preds_df.to_csv("final_preds.csv")
     # computed a different way: first, find the p/r curves, then find the area under it
 
     fpr,tpr, _ =metrics.roc_curve(all_preds_df["label"], all_preds_df["probs_1"] , 1)
@@ -32,4 +32,6 @@ def compute_auc(predictions_path: str=None):
     print(len(all_preds_df))
     print(all_preds_df)
 
-compute_auc(predictions_path="/scratch/gobi1/johnchen/new_git_stuff/multimodal_fairness/src/models/new_allen_nlp/experiments/43-fixed")
+experiments_dir = "/scratch/gobi1/johnchen/new_git_stuff/multimodal_fairness/src/models/new_allen_nlp/experiments/"
+predictions_path = os.path.join(experiments_dir, "48-200-dim-parse-line")
+compute_auc(predictions_path=predictions_path)
