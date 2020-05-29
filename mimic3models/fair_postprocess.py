0e7d183c0688498c4969400d429c19170b354286
ikb-a <ian.berlot.attwell@mail.utoronto.ca>
2020-05-29 00:39:18 -0400

    Modifies plotting code to have 3 columns

--- mimic3models/fair_postprocess.py
+++ mimic3models/fair_postprocess.py
@@ -65,6 +65,96 @@ def create_train_test_data(train_file, test_file, sensitive):
             np.array(test_X), np.array(test_score), np.array(test_Y), np.array(sens_test, dtype='<U16')
 
 
+def get_conf_base(input):
+    train_X, train_score, train_Y, sens_train, test_X, test_score, test_Y, sens_test = input
+    base_classifier = pseudo_classifier(train_X, train_Y, train_score, sens_train, test_X, test_Y, test_score,
+                                        sens_test)
+    base_classifier.fit(train_X, train_Y)
+    return base_classifier.get_group_confusion_matrix(sens_test, test_X, test_Y)
+
+def get_conf_fair(input, fairness_type):
+    train_X, train_score, train_Y, sens_train, test_X, test_score, test_Y, sens_test = input
+    base_classifier = fair_classifier(train_X, train_Y, train_score, sens_train, \
+                                             test_X, test_Y, test_score, sens_test, fairness_type)
+    base_classifier.fit()
+    return base_classifier.get_avg_group_confusion_matrix(sens_test, test_X, test_Y)
+
+def plot_line_with_offset(ax, conf_micro_macro_pairs, groups, offset, DIAMOND_THICKNESS, to_plot, i, vert_line_style, generate_legend=False):
+    for k, (model_confusion, model_micro_macro) in enumerate(conf_micro_macro_pairs):
+        max_val = -1
+        min_val = 10
+
+        for group in groups:
+            max_val = max(max_val, model_confusion[group][i])
+            min_val = min(min_val, model_confusion[group][i])
+
+        # If micro/macro statistics available, add diamond indicator
+        if to_plot[i][9:] in model_micro_macro or to_plot[
+            i] in model_micro_macro:  # TODO: Reduce Ugly hackiness
+            if to_plot[i][9:] in model_micro_macro:
+                micro, macro = model_micro_macro[to_plot[i][9:]]
+            else:
+                micro, macro = model_micro_macro[to_plot[i]]
+            ax.fill((1 + k - offset,
+                     1 + k - offset, 1 + k - offset - DIAMOND_THICKNESS),
+                    (max_val, min_val, macro),
+                    color="xkcd:light lavender")  # xkcd:light lavender
+
+            ax.fill((1 + k - offset, 1 + k - offset + DIAMOND_THICKNESS,
+                     1 + k - offset),
+                    (max_val, micro, min_val),
+                    color="xkcd:light grey")
+
+            # Add outline
+            ax.plot((1 + k - offset, 1 + k + - offset + DIAMOND_THICKNESS,
+                     1 + k - offset),
+                    (max_val, micro, min_val),
+                    color="black", linewidth=DIAMOND_LINEWIDTH)
+
+            ax.plot((1 + k - offset,
+                     1 + k - offset - DIAMOND_THICKNESS, 1 + k - offset),
+                    (max_val, macro, min_val), color="black", linewidth=DIAMOND_LINEWIDTH)
+
+            # Add star to points
+            ax.plot(1 + k - offset - DIAMOND_THICKNESS, macro, color="xkcd:light lavender",
+                    marker='o', mec="red", markersize=DIAMOND_MARKERSIZE)
+            ax.plot(1 + k - offset + DIAMOND_THICKNESS, micro, color="xkcd:light grey",
+                    marker='o', mec="black", markersize=DIAMOND_MARKERSIZE)
+
+        for j, group in enumerate(groups):
+            # draw black outline
+            ax.plot(1 + k - offset, model_confusion[group][i], marker=EN_MARK, \
+                    markersize=MARKER_SIZE + OUTLINE_THICKNESS, color="black", mew=MEW + OUTLINE_THICKNESS)
+
+            ax.plot(1 + k - offset, model_confusion[group][i], marker=U_MARK, \
+                    markersize=MARKER_SIZE, color=colors[j], mew=MEW)
+
+        ax.plot((1 + k - offset, 1 + k - offset), (min_val, max_val), color="black", linestyle=vert_line_style)
+
+
+        if generate_legend:
+            # Create Legend
+            lines = [mpl.lines.Line2D([0], [0], marker=EN_MARK,
+                                      markersize=MARKER_SIZE, color=colors[j], mew=MEW) for j, group in
+                     enumerate(groups)]
+            labels = groups.copy()
+
+            if to_plot[i][9:] in model_micro_macro or to_plot[i] in model_micro_macro:  # TODO: Reduce Ugly hackiness
+                if to_plot[i][9:] in model_micro_macro:
+                    name = to_plot[i][9:]
+                    micro, macro = model_micro_macro[to_plot[i][9:]]
+                else:
+                    name = to_plot[i]
+                    micro, macro = model_micro_macro[to_plot[i]]
+                lines += [mpl.lines.Line2D([0], [0], color=col,
+                                           marker='o', mec=mec, markersize=DIAMOND_MARKERSIZE) for col, mec in
+                          (("xkcd:light lavender", "red"), ("xkcd:light grey", "black"))]
+                labels += ["Macro " + name, "Micro " + name]
+
+            lines += [mpl.lines.Line2D([0], [0], color="black", linestyle=style) for style in ('solid', 'dashdot', 'dotted')]
+            labels += ['Unstructured', 'Structured', 'Ensemble']
+            plt.legend(lines, labels)
+
 if __name__ == "__main__":
     cmd = sys.argv[1]
     if cmd == "LOAD":
@@ -361,7 +451,7 @@ if __name__ == "__main__":
                     ax.fill((1+k-LINE_OFFSET,
                              1+k-LINE_OFFSET, 1+k - (1 + DIAMOND_THICKNESS)*LINE_OFFSET),
                             (max_val,min_val,macro),
-                            color="xkcd:light lavender") #xkcd:light lavender
+                            color="xkcd:light lavender")
 
                     ax.fill((1+k-LINE_OFFSET, 1+k + (DIAMOND_THICKNESS-1) * LINE_OFFSET,
                              1+k-LINE_OFFSET),
@@ -474,4 +564,118 @@ if __name__ == "__main__":
             plt.grid(False)
             plt.tight_layout()
             plt_name = sensitive_attr + "_" + str(to_plot[i]) + "_t3.png"
+            plt.savefig(plt_name)
+
+
+    elif cmd == "PLOT_ALL2":
+        u_train_file = sys.argv[2]
+        u_test_file = sys.argv[3]
+        s_train_file = sys.argv[4]
+        s_test_file = sys.argv[5]
+        en_train_file = sys.argv[6]
+        en_test_file = sys.argv[7]
+        sensitive_attr = sys.argv[8]
+
+        assert (sensitive_attr in ALL_SENSITIVE)
+
+        train_X, train_score, train_Y, sens_train, \
+        test_X, test_score, test_Y, sens_test = create_train_test_data(u_train_file, u_test_file, sensitive_attr)
+        en_train_X, en_train_score, en_train_Y, en_sens_train, \
+        en_test_X, en_test_score, en_test_Y, en_sens_test = create_train_test_data(en_train_file, en_test_file, sensitive_attr)
+
+        print("Train")
+        print(train_X[0], train_score[0])
+        print(en_train_X[0], en_train_score[0])
+        print("Test")
+        print(test_X[0], test_score[0])
+        print(en_test_X[0], en_test_score[0])
+
+
+        # Get Base classifier performance
+        base_confusion, base_micro_macro = get_conf_base(create_train_test_data(u_train_file, u_test_file, sensitive_attr))
+        en_base_confusion, en_base_micro_macro = get_conf_base(create_train_test_data(en_train_file, en_test_file, sensitive_attr))
+        s_base_confusion, s_base_micro_macro = get_conf_base(create_train_test_data(s_train_file, s_test_file, sensitive_attr))
+
+        dp_confusion, dp_micro_macro = get_conf_fair(create_train_test_data(u_train_file, u_test_file, sensitive_attr), "demographic_parity")
+        s_dp_confusion, s_dp_micro_macro = get_conf_fair(create_train_test_data(s_train_file, s_test_file, sensitive_attr), "demographic_parity")
+        en_dp_confusion, en_dp_micro_macro = get_conf_fair(create_train_test_data(en_train_file, en_test_file, sensitive_attr), "demographic_parity")
+
+        eo_confusion, eo_micro_macro = get_conf_fair(create_train_test_data(u_train_file, u_test_file, sensitive_attr), "equalized_odds")
+        s_eo_confusion, s_eo_micro_macro = get_conf_fair(create_train_test_data(s_train_file, s_test_file, sensitive_attr), "equalized_odds")
+        en_eo_confusion, en_eo_micro_macro = get_conf_fair(create_train_test_data(en_train_file, en_test_file, sensitive_attr), "equalized_odds")
+
+        to_plot = ['Expected TP Rate', 'Expected TN Rate', 'Expected FP Rate', 'Expected FN Rate', "Expected Accuracy",
+                   "AUC"]
+        x_axis = ["Base Classifier", "DP Classifier", "EO Classifier"]
+        colors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:gray', 'tab:olive']
+        plots = []
+        groups = base_confusion.keys()
+
+        # Fix order
+        groups = list(groups)
+
+        # Type 3 Plot
+        LINE_OFFSET = 0.2
+        MARKER_SIZE = 20
+        U_MARK = "_"
+        EN_MARK = "_"
+        MEW = 4  # Marker Edge Width
+
+        LABEL1 = True
+        LABEL2 = False
+
+        OUTLINE_THICKNESS = 2
+
+        DIAMOND_THICKNESS = 0.3 * LINE_OFFSET
+        DIAMOND_LINEWIDTH = 0.5
+        DIAMOND_MARKERSIZE = 2
+
+        for i in range(len(to_plot)):
+            fig, ax = plt.subplots()
+            ax.set_xlabel("Classifier")
+            ax.set_xticks([1, 2, 3])
+            ax.set_xticklabels(x_axis)
+            ax.set_xlim([0.5, 4.5])
+            ax.set_ylabel(to_plot[i])
+
+            # ((base_confusion, base_micro_macro), (dp_confusion, dp_micro_macro), (eo_confusion, eo_micro_macro))
+            # LINE_OFFSET
+
+            plot_line_with_offset(ax,
+                                  ((base_confusion, base_micro_macro),
+                                       (dp_confusion, dp_micro_macro),
+                                       (eo_confusion, eo_micro_macro)),
+                                  groups,
+                                  LINE_OFFSET,
+                                  DIAMOND_THICKNESS,
+                                  to_plot,
+                                  i,
+                                  'solid')
+
+            plot_line_with_offset(ax,
+                                  ((s_base_confusion, s_base_micro_macro),
+                                   (s_dp_confusion, s_dp_micro_macro),
+                                   (s_eo_confusion, s_eo_micro_macro)),
+                                  groups,
+                                  0,
+                                  DIAMOND_THICKNESS,
+                                  to_plot,
+                                  i,
+                                  'dashdot')
+
+            plot_line_with_offset(ax,
+                                  ((en_base_confusion, en_base_micro_macro),
+                                   (en_dp_confusion, en_dp_micro_macro),
+                                   (en_eo_confusion, en_eo_micro_macro)),
+                                  groups,
+                                  -1*LINE_OFFSET,
+                                  DIAMOND_THICKNESS,
+                                  to_plot,
+                                  i,
+                                  'dotted',
+                                  generate_legend=True)
+
+            plt.grid(False)
+            plt.tight_layout()
+            plt_name = sensitive_attr + "_" + str(to_plot[i]) + "_t4.png"
             plt.savefig(plt_name)
